9e70186fb29c37ad01f67a15bb87bb3540fe1c6c
Dinesh T M <37776278+tmdinesh@users.noreply.github.com>
2025-05-06 21:25:42 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -12,7 +12,7 @@ const LoginSignup = () => {
 
   const login = async () => {
     let dataObj;
-    await fetch('http://localhost:4000/login', {
+    await fetch(`${process.env.REACT_APP_API_URL}//login`, {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
@@ -35,7 +35,7 @@ const LoginSignup = () => {
 
   const signup = async () => {
     let dataObj;
-    await fetch('http://localhost:4000/signup', {
+    await fetch(`${process.env.REACT_APP_API_URL}/signup`, {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
