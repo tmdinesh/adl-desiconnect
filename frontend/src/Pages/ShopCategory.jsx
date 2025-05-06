f91083acac6040d0daab84c7243b2cc0364d1f3a
Dinesh T M <37776278+tmdinesh@users.noreply.github.com>
2025-05-06 21:27:51 +0530

    Update ShopCategory.jsx

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch(`${process.env.REACT_APP_API_URL}/allproducts`) 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
