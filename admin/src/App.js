18b9af613d30a04bfaaa783f8cbfaa10be695256
Dinesh T M <37776278+tmdinesh@users.noreply.github.com>
2025-05-06 21:47:19 +0530

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,7 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 import Admin from "./Pages/Admin";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 export const currency = '₹';
 
 function App() {
