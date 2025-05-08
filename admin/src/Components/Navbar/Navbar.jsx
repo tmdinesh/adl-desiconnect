692bf03f9416813ce71276025b9ecdefbd5638f7
Dinesh T M <37776278+tmdinesh@users.noreply.github.com>
2025-05-08 20:41:50 +0530

    Update Navbar.jsx

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../Assets/nav-logo.svg'
+import navlogo from '../Assets/nav-logo.png'
 import navprofileIcon from '../Assets/nav-profile.svg'
 
 const Navbar = () => {
